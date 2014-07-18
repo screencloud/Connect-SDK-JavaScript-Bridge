e6fc3e8e08059875de1846f0fa72c7d98bdc56a3
Luke Hubbard <luke@codegent.com>
2014-07-18 12:22:16 +0700

    added google cast config with higher inactivity timeout

--- src/connect_bridge.js
+++ src/connect_bridge.js
@@ -552,7 +552,33 @@ var connectsdk = (function () {
             window.castMessageBus = window.castReceiverManager.getCastMessageBus("urn:x-cast:com.connectsdk");
             window.castMessageBus.addEventListener("message", this.handleMessage.bind(this));
 
-            window.castReceiverManager.start();
+            var appConfig = new cast.receiver.CastReceiverManager.Config();
+
+            /**
+             * Text that represents the application status. It should meet
+             * internationalization rules as may be displayed by the sender application.
+             * @type {string|undefined}
+             **/
+            appConfig.statusText = 'ScreenCloud Ready';
+
+            /**
+             * Maximum time in seconds before closing an idle
+             * sender connection. Setting this value enables a heartbeat message to keep
+             * the connection alive. Used to detect unresponsive senders faster than
+             * typical TCP timeouts. The minimum value is 5 seconds, there is no upper
+             * bound enforced but practically it's minutes before platform TCP timeouts
+             * come into play. Default value is 10 seconds.
+             * @type {number|undefined}
+             **/
+            // 100 minutes for testing, use default 10sec in prod by not setting this value
+            appConfig.maxInactivity = 60*60*24*365*10; // 10 years
+
+            /**
+             * Initializes the system manager. The application should call this method when
+             * it is ready to start receiving messages, typically after registering
+             * to listen for the events it is interested on.
+             */
+            window.castReceiverManager.start(appConfig);
         },
 
         _setCastElement: function (element) {
